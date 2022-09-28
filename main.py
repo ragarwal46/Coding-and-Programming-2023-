aa36c2b58d9531d9259ac8bc1d69af7dc45e410c
rohcoding46 <ragarwal.edu@gmail.com>
2022-09-28 14:57:54 -0400

    pull

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-import tkinter
-from Student import Student 
-
-parti = 0
-
-addParti(parti)
-
-#TODO: Cannot import functions from other file. 
-=======
 import Student
+from Student import createNewStudent 
+import tkinter as tk
+Jeff = Student.Student("jeff", 12, 10, 4.1, 30, 2)
+partiInput = int(input("How many events did Jeff participate in? "))
+print(Jeff.parti + partiInput)
 
-# Jeff = Student.Student("jeff", 12, 10, 4.1, 30, 2)
-# partiInput = int(input("How many events did Jeff participate in? "))
-student = Student.Student(Student.createNewStudent())
->>>>>>> 1e83bec3af55c09f65152de922addf96d5b65710
-
-print(student.name)
-
-student.addParti()
